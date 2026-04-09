c76d286c5c9cea1eeee1eda923474c30631c8f43
vrybachenko <rybachenko.vadim@gmail.com>
2026-04-09 20:39:29 +0300

    update logic

--- config/global-setup.js
+++ config/global-setup.js
@@ -6,9 +6,10 @@ module.exports = async () => {
     const env = process.env.ENV || 'qauto1';
     const resultsDir = path.join(__dirname, '..', `allure-results-${env}`);
 
-    if (!fs.existsSync(resultsDir)) {
-        fs.mkdirSync(resultsDir, { recursive: true });
+    if (fs.existsSync(resultsDir)) {
+        fs.rmSync(resultsDir, { recursive: true });
     }
+    fs.mkdirSync(resultsDir, { recursive: true });
 
     fs.writeFileSync(
         path.join(resultsDir, 'environment.properties'),
